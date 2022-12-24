0333b4df97284082b1aeb6f3cc467c243b12f58c
kadekbintang <bintang@mtn-fm.id>
2022-12-24 15:04:53 +0700

    add cache in get article

--- app/routers/router.go
+++ app/routers/router.go
@@ -2,13 +2,17 @@ package routers
 
 import(
 	"github.com/gin-gonic/gin"
+	"github.com/chenyahui/gin-cache"
+	"github.com/chenyahui/gin-cache/persist"
 	"net/http"
 	"testgolang/app/handler"
+	"time"
 )
 
 func InitRouter(){
 	ArticleHandler := handler.NewArticleHandler()
 	r := gin.Default()
+	memoryStore := persist.NewMemoryStore(1 * time.Minute)
 	api := r.Group("api/v1")
 
 	api.GET("/health", func(c *gin.Context){
@@ -19,7 +23,7 @@ func InitRouter(){
 
 	api.GET("/article/health", ArticleHandler.HealthArticle)
 	api.POST("/article", ArticleHandler.CreateArticle)
-	api.GET("/article", ArticleHandler.GetArticle)
+	api.GET("/article", cache.CacheByRequestURI(memoryStore, 60*time.Second), ArticleHandler.GetArticle)
 
 
 	r.Run()
